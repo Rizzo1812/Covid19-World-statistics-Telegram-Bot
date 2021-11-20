af3e7ac3a58eb1c66e31f74dc1b2c82dc0322d06
Antonio Rizzo <rizzo.antonioa7000@gmail.com>
2021-11-20 01:15:19 +0100

    Better API provider

--- README.md
+++ README.md
@@ -7,9 +7,13 @@ Telegram Bot that send you covid19 statistics (confirmed, recovered, deaths etc.
 /info - more info and credits
 
 ##  More info
-This BOT uses free APIs provided by RapidAPI
-https://rapidapi.com/
+This BOT uses free API provided by VACCOVID(https://vaccovid.live/) which can be found on RapidAPI(https://rapidapi.com/)\
 
-# Bot Showcase
+Terms legend:\
+Infection percentage: Total Number of covid-19 cases divided by Total Population since the beginning of outbreak\
+Test percentage: Total number of tests divided by total population\
+Recovery percentage: Total number of recovered cases divided by Total number of covid-19 cases\
+Fatality rate: Total Number of Deaths due to Covid-19 divided by Total Number of confirmed cases since the beginning\ of outbreak (It shows that how lethal covid-19 is in any country)\
+
+## Bot Showcase
 ![BOT SHOWCASE](https://i.postimg.cc/3NFJ7srQ/showcase.jpg)
-
--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import requests
-import json
-import time
 import os
+import flag
 import logging
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import (
@@ -13,53 +12,74 @@ from telegram.ext import (
 )
 
 countries = list()
-country_keyboard = None
 
 # Header of the requests
 headers = {
-    'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
+    'x-rapidapi-host': "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com",
     'x-rapidapi-key': os.environ['RAPIDAPIKEY']
     }
 
-#divide list of countries in chuncks of 2
+# Split list of countries in chuncks of n
 def divide_chunks(l, n):
     for i in range(0, len(l), n): 
         yield l[i:i + n]
 
 # Create the list containing all countries 
-def create_country_list():
-    global country_keyboard
-    jsonfile = open('ISO3166.json',encoding='UTF-8')
-    data = json.load(jsonfile)
-    for country in data:
-        countries.append("{} {} ({})".format(country['emojiFlag'], country['country'], country['isoCode']))
-    country_keyboard = list(divide_chunks(countries, 2))
-
-# Get iso code of a country 
-def get_iso(country_string):
-    return country_string[country_string.find("(")+1:country_string.find(")")]
+def get_country_keyboard(response):
+    response = response.json()
+    global countries
+    list_of_countries = list()
+    for country in response:
+        list_of_countries.append("{} {}".format(country['Country'], flag.flag(country['TwoLetterSymbol'])))
+    countries = list_of_countries
+    countries.sort()
+    return list(divide_chunks(countries, 2))
 
 # Build the message to send to user with statistics
-def build_response_message(response, country="World"):
-    response = response.json()[0]
-    if country != "World" and response["lastUpdate"] is None:
-        return "Data not available for " + response["country"]
+def build_world_response_message(response):
+    world_stats = response.json()[0]
 
-    if country == "World":
-        message = "🌎 World:\n\n"
-    else:
-        message = country[0:2] + " " + response["country"] + ":\n\n"
-    message += "🦠  Positive cases: "+str(f'{response["confirmed"]:,}') + "\n" +\
-    "❤️‍🩹 Recovered: "+str(f'{response["recovered"]:,}') + "\n" +\
-    "🏥  Critical cases: "+str(f'{response["critical"]:,}') + "\n" +\
-    "☠️  Deaths: "+str(f'{response["deaths"]:,}') + "\n"
+    message = "🌎  World:\n\n" +\
+    "📊  Total\n" +\
+    "🦠  Total cases: "+str(f'{world_stats["TotalCases"]:,}') + "\n" +\
+    "❤️‍🩹  Total recovered: "+str(f'{int(world_stats["TotalRecovered"]):,}') + "\n" +\
+    "🏥  Critical cases: "+str(f'{world_stats["Serious_Critical"]:,}') + "\n" +\
+    "☠️  Total deaths: "+str(f'{world_stats["TotalDeaths"]:,}') + "\n\n" +\
+    "📈  Daily\n" +\
+    "🤒  Active Cases: "+str(f'{world_stats["ActiveCases"]:,}') + "\n" +\
+    "🦠  New cases: "+str(f'{world_stats["NewCases"]:,}') + "\n" +\
+    "❤️‍🩹  New recovered: "+str(f'{world_stats["NewRecovered"]:,}') + "\n" +\
+    "☠️  New deaths: "+str(f'{world_stats["NewDeaths"]:,}')
     
     return message
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+# Build the message to send to user with statistics
+def build_country_response_message(response, selected_country):
+    country_stats = [country for country in response if country['Country'] == selected_country[:-3]][0]
 
+    message = selected_country + ":\n\n" +\
+    "🗺️  Country informations\n" +\
+    "👥  Population: "+str(f'{country_stats["Population"]:,}') + "\n" +\
+    "🤒  Infection risk: "+str(f'{country_stats["Infection_Risk"]:,}') + "%\n" +\
+    "🔬  Test percentage: "+str(f'{country_stats["Test_Percentage"]:,}') + "%\n" +\
+    "❤️‍🩹  Recovery percentage: "+str(f'{country_stats["Recovery_Proporation"]:,}') + "%\n" +\
+    "☠️  Fatality rate: "+str(f'{country_stats["Case_Fatality_Rate"]:,}') + "%\n\n" +\
+    "📊  Total\n" +\
+    "🦠  Total cases: "+str(f'{country_stats["TotalCases"]:,}') + "\n" +\
+    "❤️‍🩹  Total recovered: "+str(f'{int(country_stats["TotalRecovered"]):,}') + "\n" +\
+    "🔬  Total tests: "+str(f'{int(country_stats["TotalTests"]):,}') + "\n" +\
+    "🏥  Critical cases: "+str(f'{country_stats["Serious_Critical"]:,}') + "\n" +\
+    "☠️  Total deaths: "+str(f'{country_stats["TotalDeaths"]:,}') + "\n\n" +\
+    "📈  Daily\n" +\
+    "🦠  New cases: "+str(f'{country_stats["NewCases"]:,}') + "\n" +\
+    "🤒  Active Cases: "+str(f'{country_stats["ActiveCases"]:,}') + "\n" +\
+    "❤️‍🩹  New recovered: "+str(f'{country_stats["NewRecovered"]:,}') + "\n" +\
+    "☠️  New deaths: "+str(f'{country_stats["NewDeaths"]:,}')
+
+    return message
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Handle /start command
@@ -73,6 +93,7 @@ def start(update, context):
         "/info - More info and credits")
     update.message.reply_text(welcome_message)
 
+# Handle /info command
 def info(update, contex):
     """Send a message with more info about the BOT when the command /info is issued."""
     info_message = ("Welcome to Covid-19 World Statistics 👋\n"
@@ -82,49 +103,46 @@ def info(update, contex):
         "/countrystats - Get latest Covid-19 statistic of a specific country\n"
         "/info - More info and credits\n\n\n"
         "ℹ️ MORE INFO ℹ️\n"
-        "This BOT uses free APIs provided by RapidAPI\n"
-        "https://rapidapi.com/\n\n"
         "The code of this BOT is open source\n"
         "Source code is on GitHub: https://github.com/Rizzo1812/Covid19-World-statistics-Telegram-Bot\n\n"
+        "🤒 Infection percentage: Total Number of covid-19 cases divided by Total Population since the beginning of outbreak\n"
+        "🔬 Test percentage: Total number of tests divided by total population\n"
+        "❤️‍🩹 Recovery percentage: Total number of recovered cases divided by Total number of covid-19 cases\n"
+        "☠️ Fatality rate: Total Number of Deaths due to Covid-19 divided by Total Number of confirmed cases since the beginning of outbreak (It shows that how lethal covid-19 is in any country)\n\n"
         "©️ Credits ©️\n"
-        "This BOT uses a list of all countries made by selimata (GitHub user)\n"
-        "https://gist.github.com/selimata")
+        "This BOT uses free API provided by VACCOVID(https://vaccovid.live/) which can be found on RapidAPI")
     update.message.reply_text(info_message)
 
-# Handle /worldstats command (send Covid19 global statistics)
+# Handle /worldstats command
+def worldstats(update, context):
+    response = requests.request("GET", "https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/world", headers=headers)    
+    
+    update.message.reply_text(build_world_response_message(response))
+    return ConversationHandler.END
+
+# Handle /countrystats command
 def countrystats(update, context):
+    response = requests.request("GET", "https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/countries", headers=headers)
+    country_keyboard = get_country_keyboard(response)
+
     update.message.reply_text(
-        'Now select the country for which you want to get statistics ',
+        'Now select the country for which you want to get statistics',
         reply_markup=ReplyKeyboardMarkup(
             country_keyboard, one_time_keyboard=True, input_field_placeholder='Select country'
         ),
     )
     return 1
 
-# Handle /countrystats command
-def worldstats(update, context):
-    response = requests.request("GET", "https://covid-19-data.p.rapidapi.com/totals", headers=headers)    
-    while response.status_code != 200:
-        time.sleep(1.1)
-        response = requests.request("GET", "https://covid-19-data.p.rapidapi.com/totals", headers=headers) 
-    
-    update.message.reply_text(build_response_message(response))
-    return ConversationHandler.END
-
 # Send Covid19 statistics of a country
 def country_report(update, context):
     if update.message.text not in countries:
-        update.message.reply_text('Sorry this is not a valid Country',reply_markup = ReplyKeyboardRemove())
+        update.message.reply_text('This is not a valid Country',reply_markup = ReplyKeyboardRemove())
         return ConversationHandler.END
 
-    querystring = {"code":get_iso(update.message.text),"format":"json"}
-    response = requests.request("GET", "https://covid-19-data.p.rapidapi.com/country/code", headers=headers, params=querystring)    
-    while response.status_code != 200:
-        time.sleep(1.1)
-        response = requests.request("GET", "https://covid-19-data.p.rapidapi.com/country/code", headers=headers, params=querystring)    
-
+    response = requests.request("GET", "https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/countries", headers=headers).json()
+    
     update.message.reply_text(
-        build_response_message(response,update.message.text),
+        build_country_response_message(response,update.message.text),
         reply_markup = ReplyKeyboardRemove()
     )
     return ConversationHandler.END
@@ -134,11 +152,11 @@ def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+# BOT start
 def main():
     """Start the bot."""
     # Binding bot to code
     updater = Updater(os.environ['TELEGRAMAPIKEY'], use_context=True)
-    create_country_list()
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
